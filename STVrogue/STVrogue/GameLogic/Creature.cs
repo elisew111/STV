cfb45af9633d8b936674f5e15deaf50cb1db848d
Oscar Müller <okkibro@gmail.com>
2019-05-13 11:16:41 +0200

    Creature en Item voor groot gedeelte af

--- STVrogue/STVrogue/GameLogic/Creature.cs
+++ STVrogue/STVrogue/GameLogic/Creature.cs
@@ -1,24 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace STVrogue.GameLogic
-{
-    public class Creature : GameEntity
-    {
+namespace STVrogue.GameLogic {
+    public class Creature : GameEntity {
 
         public String name = "goblin";
         public int HP = 1;     // current HP, should never exceed HPmax
         public Node location;
-        public int AttackRating = 1;
+        public int attackRating = 1;
 
-        public Creature(String ID) : base(ID){ }
+        public Creature(String ID) : base(ID) { }
 
         /*
          * Attack the foe. This one just throws an exception; you need to
          * override it in the corresponding subclasses (Monster and Player).
          */
-        public virtual void attack(Game G, Creature foe)
-        {
+        public virtual void Attack(Game G, Creature foe) {
             throw new NotImplementedException();
         }
 
@@ -28,8 +26,7 @@ namespace STVrogue.GameLogic
          * This one just throws an exception; you need to
          * override it in the corresponding subclasses (Monster and Player).
          */
-        public virtual Boolean move(Game G, Node nd)
-        {
+        public virtual Boolean Move(Game G, Node nd) {
             throw new NotImplementedException();
         }
 
@@ -39,93 +36,147 @@ namespace STVrogue.GameLogic
          * This one just throws an exception; you need to
          * override it in the corresponding subclasses (Monster and Player).
          */
-        public virtual Boolean flee(Game G, Node nd)
-        {
+        public virtual Boolean Flee(Game G, Node nd) {
             throw new NotImplementedException();
         }
     }
 
-    public class Monster : Creature
-    {
-        public Monster(String ID) : base(ID)
-        {
-            // you need to decide how to initialize the other attributes
-            throw new NotImplementedException();
+    public class Monster : Creature {
+
+        /* Monster has a maximum of 5 HP */
+        int HPmax = 5;
+        public Monster(String ID) : base(ID) {
+            // Add position initialization (accounting for capacity of node).
+            HP = HPmax;
+            attackRating = 1;
+            name = "orc";
         }
-       
+
         /*
          * Attack the foe. 
          */
-        public override void attack(Game G, Creature foe)
-            {
-                throw new NotImplementedException();
-            }
+        public override void Attack(Game G, Creature foe) {
+            foe.HP -= attackRating;
+            // Check if player's HP is below 0 and kill the player if this is the case.
+        }
 
-    
+
         /*
          * Move this monster to the given node. Return true if the move is
          * successful, else false.
          */
-        public override Boolean move(Game G, Node nd)
-            {
-                throw new NotImplementedException();
-            }
+        public override Boolean Move(Game G, Node nd) {
+            // Node monster wants to move to is not a neighbouring node.
+            List<Node> possibleMoves = nd.neighbors;
+            if (!possibleMoves.Contains(this.location))
+                return false;
+            // Node monster wants to move to is not in the same zone.
+            Zone currentZone = this.location.zone;
+            if (currentZone != nd.zone)
+                return false;
+            // Update monster's location and return true.
+            this.location = nd;
+            return true;
+        }
 
         /*
          * This monster flees to the given node. Return true is this is successful,
          * else false.
          */
-        public override Boolean flee(Game G, Node nd)
-            {
-                throw new NotImplementedException();
-            }
+        public override Boolean Flee(Game G, Node nd) {
+            // Node monster wants to move to is not a neighbouring node.
+            List<Node> possibleMoves = nd.neighbors;
+            if (!possibleMoves.Contains(this.location))
+                return false;
+            // Node monster wants to move to is not in the same zone.
+            Zone currentZone = this.location.zone;
+            if (currentZone != nd.zone)
+                return false;
+            // Node monster wants to move to already contains the player.
+            Node playerLocation = G.player.location;
+            if (nd != playerLocation)
+                return false;
+            // Update monster's location and return true.
+            this.location = nd;
+            return true;
+        }
     }
 
-    public class Player : Creature
-    {
-        /** kill point */
+    public class Player : Creature {
+
+        // Player starts with having made 0 kills
         int KP = 0;
-        int HPmax;
+        // Player has a maximum of 10 HP
+        public int HPmax = 10;
         public Boolean boosted = false;
         public Boolean inCombat = false;
+        // Player starts with an empty bag
         public List<Item> bag = new List<Item>();
 
-        public Player(String ID) : base(ID)
-        {
-            // you need to decide how to initialize the other attributes
-            throw new NotImplementedException();
+        public Player(String ID) : base(ID) {
+            // Add position initialization (start node)
+            HP = HPmax;
+            name = "player";
+            attackRating = 2;
         }
 
         /*
          * Attack the foe. 
          */
-        public override void attack(Game G, Creature foe)
-        {
-            throw new NotImplementedException();
+        public override void Attack(Game G, Creature foe) {
+            // Decrease foe's HP by player's attackrating
+            foe.HP -= G.player.attackRating;
+            // Add to kill count if player defeats foe.
+            if (foe.HP <= 0)
+                G.player.KP++;
         }
 
         /*
          * Move the player to the given node. Return true if the move is
          * successful, else false.
          */
-        public override Boolean move(Game G, Node nd)
-        {
-            throw new NotImplementedException();
+        public override Boolean Move(Game G, Node nd) {
+            // Node player wants to move to is not a neighbouring node.
+            List<Node> possibleMoves = nd.neighbors;
+            if (!possibleMoves.Contains(G.player.location))
+                return false;
+            // Update player's location.
+            G.player.location = nd;
+            // Add any items in new node to player's bag.
+            List<Item> nodeItems = nd.items;
+            if (!nodeItems.Any()) {
+                foreach(Item item in nodeItems)
+                    G.player.bag.Add(item);
+            }
+            // Set 'inCombat' to true if new node contains a monster.
+            List<Creature> nodeCreatures = nd.monsters;
+            if (!nodeCreatures.Any())
+                G.player.inCombat = true;
+            return true;
         }
 
         /*
          * The player flees to the given node. Return true is this is successful,
          * else false.
          */
-        public override Boolean flee(Game G, Node nd)
-        {
-            throw new NotImplementedException();
+        public override Boolean Flee(Game G, Node nd) {
+            // Node player wants to move to is not a neighbouring node.
+            List<Node> possibleMoves = nd.neighbors;
+            if (!possibleMoves.Contains(G.player.location))
+                return false;
+            // Node player wants to flee to already contains a monster.
+            List<Creature> nodeCreatures = nd.monsters;
+            if (!nodeCreatures.Any())
+                return false;
+            // Update player's location.
+            G.player.location = nd;
+            // Add any items in new node to player's bag.
+            List<Item> nodeItems = nd.items;
+            if (!nodeItems.Any()) {
+                foreach (Item item in nodeItems)
+                    G.player.bag.Add(item);
+            }
+            return true;
         }
-
-
     }
-
-
-
-
 }
--- STVrogue/STVrogue/GameLogic/Item.cs
+++ STVrogue/STVrogue/GameLogic/Item.cs
@@ -1,50 +1,103 @@
 ﻿using System;
-namespace STVrogue.GameLogic
-{
-    public class Item : GameEntity
-    {
-        public Item(String ID) : base(ID){ }
+
+namespace STVrogue.GameLogic {
+
+    public class Item : GameEntity {
+
+        public Item(String ID) : base(ID) { }
 
         /*
          * Implementing the logic of what happen when the player uses this item.
          * Here it does nothing. Override this accordingly in the subclasses.
          */
-        public virtual void use(Game G, Player player) { }
+        public virtual void Use(Game G, Player player) { }
     }
 
-    public class HealingPotion : Item
-    {
-        /* it can heal this many HP */
-        int HPvalue;
+    public class HealingPotion : Item {
 
-        public HealingPotion(String ID, int heal) : base(ID)
-        {
+        /* HealingPotion can heal 5 HP*/
+        int HPvalue = 5;
+
+        public HealingPotion(String ID, int heal) : base(ID) {
             this.HPvalue = heal;
         }
 
         /*
          * Using a healing potion heals the player (but not beyond his HPmax).
          */
-        public override void use(Game G, Player player)
-        {
-            throw new NotImplementedException();
+        public override void Use(Game G, Player player) {
+            if(hasHealingPotion(player)) {
+                player.HP = Math.Min(player.HPmax, player.HP + this.HPvalue);
+                deleteHealingPotion(player);
+            } 
+        }
+
+        public bool hasHealingPotion(Player player) {
+            foreach (Item item in player.bag) {
+                if (item is HealingPotion)
+                    return true;
+            }
+            return false;
+        }
+
+        public void deleteHealingPotion(Player player) {
+            String itemToDeleteID = "";
+            int bagSize = player.bag.Count;
+            foreach (Item item in player.bag) {
+                if (item is HealingPotion) {
+                    itemToDeleteID = item.ID;
+                    break;
+                }
+            }
+            for (int i = 0; i <= bagSize; i++) {
+                if (player.bag[i].ID == itemToDeleteID) {
+                    player.bag.RemoveAt(i);
+                    break;
+                }
+            }
         }
     }
 
-    public class Crystal : Item
-    {
+    public class Crystal : Item {
 
-        public Crystal(String ID) : base(ID){ }
+        public Crystal(String ID) : base(ID) { }
 
         /*
          * Using a crystal during a combat temporarily doubles the player's 
          * attack rating. The effect is gone once the combat ends.
          * Using a crystal while not in combat has no effect.
          */
-        public override void use(Game G, Player player)
-        {
-            throw new NotImplementedException();
+        public override void Use(Game G, Player player) {
+            if(hasCrystal(player)) {
+                deleteCrystal(player);
+                if(player.inCombat)
+                    player.boosted = true;
+            }   
+        }
+
+        public bool hasCrystal(Player player) {
+            foreach (Item item in player.bag) {
+                if (item is HealingPotion)
+                    return true;
+            }
+            return false;
+        }
+
+        public void deleteCrystal(Player player) {
+            String itemToDeleteID = "";
+            int bagSize = player.bag.Count;
+            foreach (Item item in player.bag) {
+                if (item is Crystal) {
+                    itemToDeleteID = item.ID;
+                    break;
+                }
+            }
+            for (int i = 0; i <= bagSize; i++) {
+                if (player.bag[i].ID == itemToDeleteID) {
+                    player.bag.RemoveAt(i);
+                    break;
+                }
+            }
         }
     }
-
 }
