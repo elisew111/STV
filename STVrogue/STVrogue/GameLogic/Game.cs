3497f343d5c21117c1f7c5a0a3f56e5800567631
WolfVisions <mircopirco@hotmail.com>
2019-05-19 11:02:52 +0200

    Basic logic Game.cs, some things missing
    How to determine if the players node has monsters?
    How to move between nodes?

--- STVrogue/STVrogue/GameLogic/Game.cs
+++ STVrogue/STVrogue/GameLogic/Game.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using STVrogue.GameControl;
 
@@ -16,12 +16,13 @@ namespace STVrogue.GameLogic
     /* This class represents the whole game state of STV-Rogue */
     public class Game
     {
-
         public Player player;
         /* all monsters currently live in the game. */
         public List<Monster> monsters = new List<Monster>();
         /* all items in the game */
         public List<Item> items = new List<Item>();
+        public Crystal crystal;
+        public HealingPotion healingPotion;
         /* The dungeon */
         public Dungeon dungeon;
 
@@ -29,6 +30,8 @@ namespace STVrogue.GameLogic
         public int turnNumber = 0;
         /* The creature that currently has the turn. */
         public Creature whoHasTheTurn;
+
+
 
         public PlayerState playerstate;
 
@@ -42,7 +45,9 @@ namespace STVrogue.GameLogic
          */
         public Game(int level, int capacityMultiplier)
         {
-            throw new NotImplementedException();
+            Dungeon dungeon = new Dungeon(6, 3);
+            Player player = new Player("player1");
+            //throw new NotImplementedException();
         }
 
         /* return all nodes in the game. */
@@ -75,7 +80,35 @@ namespace STVrogue.GameLogic
          */
         public Boolean doNCTurn(Creature C, Command cmd)
         {
-            throw new NotImplementedException();
+			String s = cmd.returnArgs();
+            switch(cmd) 
+            {
+                case CommandType.MOVE:
+                    if(C is Player || C is Monster)
+                    {
+                        if(s != "none")
+                            return C.Move(this, ); //TODO: wat in te vullen om te bewegen naar een volgende node????
+                        else
+                            throw Exception();
+                    }
+                    else return false;
+                case CommandType.DoNOTHING:
+                    if(C is Player || C is Monster)
+                        return true;
+                    else return false;
+                case CommandType.USE:
+                    if(C is Player)
+                    {
+                        if(s == "potion")
+                            healingPotion.Use(this, C);
+                        else if (s == "crystal")
+                            crystal.Use(this, C);
+                        return true;
+                    }                   
+                    else return false;
+                default:
+                    throw new Exception("Ongeldig command");
+            }
         }
 
         /*
@@ -91,8 +124,84 @@ namespace STVrogue.GameLogic
          * false.
          */
         public Boolean doOneCombatRound(Command cmd)
+        {   
+            if(player.inCombat == true)
+            {
+                if(player.boosted == true)
+                {
+                    playerstate = PlayerState.CombatStartAndBoosted;
+                    return doAction(cmd);
+                }
+                else
+                {
+                    playerstate = PlayerState.CombatStart;
+                    return doAction(cmd);
+                }
+            }
+        }
+
+        public Boolean doAction(Command cmd)
         {
-            throw new NotImplementedException();
+            String s = cmd.returnArgs();
+            switch(cmd.name)
+                {
+                    case CommandType.ATTACK:
+                        player.Attack(this, ); //How to find where a monster is?
+                        return routineAfterAttack();
+                    case CommandType.FLEE:
+                        if(player.Flee(this, ) == true)//How to determine which node?
+                        {
+                            playerstate = PlayerState.CombatEnd; player.boosted = false; player.inCombat = false;
+                        }                        
+                        else
+                        {
+                            //What to do if a flee is unsuccesful?
+                        }
+                        break;
+                    case CommandType.USE:
+                        if(s == "potion")
+                        {
+                            healingPotion.Use(this, C);
+                            playerstate = PlayerState.CombatCommitted;
+                            player.Attack(this, );//how to determine which monster?
+                            return routineAfterAttack();
+                        }
+                        else if (s == "crystal")
+                        {
+                            crystal.Use(this, C);
+                            playerstate = PlayerState.CombatComittedAndBoosted;
+                            boosted = true;
+                            player.Attack(this, );//how to determine which monster?
+                            return routineAfterAttack();
+                        }
+                        else
+                            throw Exception();
+                        break;
+                }
+        }
+        
+        public Boolean routineAfterAttack()
+        {
+            if(/*amount of monsters in current node is zero*/)
+            {
+                playerstate = PlayerState.CombatEnd; player.boosted = false; player.inCombat = false;
+                return true;
+            }
+
+            //player dead??? nothing specifies what happens if the player is dead.
+
+            if(player.boosted == true)
+            {
+                //TODO: monster turns
+                playerstate = PlayerState.CombatStartAndBoosted;
+                return false;
+            }
+            else
+            {
+                //TODO: monster turns
+                playerstate = PlayerState.CombatStart;
+                return false;
+            }
         }
     }
 }
