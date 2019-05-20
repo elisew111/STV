107a5cf4f30388d2a1572f8e09590829f02070c2
WolfVisions <mircopirco@hotmail.com>
2019-05-20 14:22:05 +0200

    Potion tests

--- STVrogue/MSUnitTests/HealingPotionTests.cs
+++ STVrogue/MSUnitTests/HealingPotionTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using STVrogue.GameLogic;
 using System;
 using System.Collections.Generic;
@@ -6,6 +6,70 @@ using System.Collections.Generic;
 namespace MSUnitTests {
     [TestClass]
     public class HealingPotionTests {
+
+        [TestMethod]
+        public void TestHasHealingPotionTrue()
+        {
+            Player player = new Player("player");
+            HealingPotion healingPotion = new HealingPotion("potion", 5);
+            player.bag.Add(healingPotion);
+            Assert.IsTrue(healingPotion.hasHealingPotion(player));
+        }
+
+        [TestMethod]
+        public void TestHasHealingPotionFalse()
+        {
+            Player player = new Player("player");
+            HealingPotion healingPotion = new HealingPotion("potion", 5);
+            Assert.IsFalse(healingPotion.hasHealingPotion(player));
+        }
+
+        [TestMethod]
+        public void TestUseNotMaxHP()
+        {
+            Game game = new Game();
+            Player player = new Player("player");
+            player.HP = 2;
+            HealingPotion healingPotion = new HealingPotion("potion", 5);
+            player.bag.Add(healingPotion);
+            healingPotion.Use(game, player);
+            Assert.IsTrue(!healingPotion.hasHealingPotion(player) && player.HP == 7);            
+        }
+
+        [TestMethod]
+        public void TestUseNotMaxHP2()
+        {
+            Game game = new Game();
+            Player player = new Player("player");
+            player.HP = 7;
+            HealingPotion healingPotion = new HealingPotion("potion", 5);
+            player.bag.Add(healingPotion);
+            healingPotion.Use(game, player);
+            Assert.IsTrue(!healingPotion.hasHealingPotion(player) && player.HP == player.HPmax);
+        }
+
+        [TestMethod]
+        public void TestUseMaxHP()
+        {
+            Game game = new Game();
+            Player player = new Player("player");
+            player.HP = player.HPmax;
+            int prevHP = player.HP;
+            HealingPotion healingPotion = new HealingPotion("potion", 5);
+            player.bag.Add(healingPotion);
+            healingPotion.Use(game, player);
+            Assert.IsTrue(!healingPotion.hasHealingPotion(player) && player.HP == player.HPmax);
+        }
+
+        [TestMethod]
+        public void TestDelete()
+        {
+            Player player = new Player("player");
+            HealingPotion healingPotion = new HealingPotion("potion", 5);
+            player.bag.Add(healingPotion);
+            healingPotion.deleteHealingPotion(player);
+            Assert.IsTrue(healingPotion.hasHealingPotion(player));
+        }
 
     }
 }
